b686beb71925df499b32b4885ab0e298a7876a95
LIN-Tianyuan <47159399+LIN-Tianyuan@users.noreply.github.com>
2025-10-11 15:03:39 +0200

    week 18

--- Chapter6/Course/16_dictionary.py
+++ Chapter6/Course/16_dictionary.py
@@ -35,9 +35,52 @@ stu_score3 = {
     "Alex": 77
 }
 
+# Modifier l'élément
 stu_score3["Lucy"] = 90
 print(stu_score3)
 
+# Ajouter un élément
+stu_score3["Luna"] = 66
+print(stu_score3)
 
+# Supprimer un élément
+score_luna = stu_score3.pop("Luna")
+print(score_luna)
+print(stu_score3)
 
+stu_score3.clear()
+print(stu_score3)
 
+stu_score4 = {
+    "Léon": 99,
+    "Lucy": 88,
+    "Alex": 77
+}
+
+# Obtenir toutes les clés
+keys = stu_score4.keys()
+print(keys)
+
+for key in keys:
+    print(key)
+
+# Objenir toutles les valeurs
+values = stu_score4.values()
+for value in values:
+    print(value)
+
+print("----------")
+# Itération de dictionnaire
+stu_score5 = {
+    "Léon": 99,
+    "Lucy": 88,
+    "Alex": 77
+}
+
+keys = stu_score5.keys()
+for key in keys:
+    print(f"{key} {stu_score5[key]}")
+
+print("----------")
+# Calculer le nombre de tous les éléments
+print(len(stu_score5))
